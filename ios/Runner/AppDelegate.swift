ebf8ed9159c95d41dbc072cbd47e596de4addf83
KagurazakaHanabi <i@yaerin.com>
2019-03-15 15:49:39 +0800

    Support iOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,10 +8,38 @@ import Photos
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
     ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    PHPhotoLibrary.requestAuthorization({status in
-        // do nothing.
+    let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
+    let channel = FlutterMethodChannel(name: "ml.cerasus.pics", binaryMessenger: controller)
+    channel.setMethodCallHandler({
+        (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+        guard call.method == "syncGallery" else {
+            result(FlutterMethodNotImplemented)
+            return
+        }
+        self.syncGallery(file: call.arguments as! String, result: result)
     })
+    GeneratedPluginRegistrant.register(with: self)
+    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+    
+    private func syncGallery(file: String, result: @escaping FlutterResult) {
+        PHPhotoLibrary.requestAuthorization({status in
+            if status == .authorized {
+                PHPhotoLibrary.shared().performChanges({
+                    PHAssetChangeRequest.creationRequestForAssetFromImage(atFileURL: URL.init(string: file)!)
+                }, completionHandler: { (success: Bool, error: Error?) in
+                    if success {
+                        result(nil) // 因为是 iOS 所以不需要返回相册中的路径
+                    } else {
+                        result(FlutterError(code: "Permission Denied", message: nil, details: nil))
+                        
+                    }
+                })
+                result("")
+            } else {
+                result(FlutterError(code: "Permission Denied", message: nil, details: nil))
+            }
+        })
+    }
 }
