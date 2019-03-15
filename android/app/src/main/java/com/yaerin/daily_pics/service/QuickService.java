3045b80f0e9564a30aa773a8bb4283e4ea970ea3
KagurazakaHanabi <i@yaerin.com>
2019-03-15 17:52:00 +0800

    Support Android N Tile

--- android/app/src/main/java/com/yaerin/daily_pics/service/QuickService.java
+++ android/app/src/main/java/com/yaerin/daily_pics/service/QuickService.java
@@ -1,10 +1,25 @@
 package com.yaerin.daily_pics.service;
 
+import android.app.WallpaperManager;
 import android.os.Build;
+import android.os.Handler;
+import android.os.Looper;
 import android.service.quicksettings.TileService;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
+import com.yaerin.daily_pics.R;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.List;
+
 import androidx.annotation.RequiresApi;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.ResponseBody;
 
 /**
  * Create by Yaerin on 2019/3/2
@@ -12,9 +27,75 @@ import androidx.annotation.RequiresApi;
  * @author Yaerin
  */
 @RequiresApi(api = Build.VERSION_CODES.N)
-public class QuickService extends TileService {
+public class QuickService extends TileService implements Runnable {
     @Override
     public void onClick() {
-        Toast.makeText(this, "暂未实现", Toast.LENGTH_SHORT).show();
+        new Thread(this).start();
+    }
+
+    @Override
+    public void run() {
+        try {
+            toast("正在开始下载");
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url("https://dp.chimon.me/api/random.php?api=yes")
+                    .build();
+            okhttp3.Response response = client.newCall(request).execute();
+            ResponseBody body = response.body();
+            if (body == null) {
+                throw new IOException("ERR_EMPTY_RESPONSE");
+            }
+            Response res = new Gson().fromJson(body.string(), Response.class);
+            String imageUrl = res.data.get(0).url;
+            URL url = new URL(imageUrl);
+            InputStream is = url.openConnection().getInputStream();
+            WallpaperManager manager = (WallpaperManager) getSystemService(WALLPAPER_SERVICE);
+            manager.setStream(is);
+            toast("下载完成");
+
+        } catch (IOException e) {
+            toast(getString(R.string.err_set_failed, e.getLocalizedMessage()));
+        }
+    }
+
+    private void toast(String text) {
+        new Handler(Looper.getMainLooper()).post(() -> {
+            Toast.makeText(QuickService.this, text, Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    class Response {
+        @SerializedName("pictures")
+        List<Picture> data;
+
+        String status;
+    }
+
+    class Picture {
+        @SerializedName("PID")
+        String id;
+
+        @SerializedName("p_title")
+        String title;
+
+        @SerializedName("p_content")
+        String info;
+
+        int width;
+
+        int height;
+
+        @SerializedName("username")
+        String user;
+
+        @SerializedName("p_link")
+        String url;
+
+        @SerializedName("p_date")
+        String date;
+
+        @SerializedName("TNAME")
+        String type;
     }
 }
