b3b91102d93e81b20b57bf85cc47f7115320a257
KagurazakaHanabi <i@yaerin.com>
2019-11-05 14:48:31 +0800

    Migrate to the Android embedding v2

--- android/app/src/main/java/com/yaerin/daily_pics/MainActivity.java
+++ android/app/src/main/java/com/yaerin/daily_pics/MainActivity.java
@@ -16,16 +16,18 @@
 
 package com.yaerin.daily_pics;
 
-import android.os.Bundle;
+import androidx.annotation.NonNull;
 
-import io.flutter.app.FlutterActivity;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.embedding.engine.plugins.shim.ShimPluginRegistry;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        GeneratedPluginRegistrant.registerWith(this);
-        PlatformPlugin.registerWith(registrarFor("ml.cerasus.pics"));
+    public void configureFlutterEngine(@NonNull FlutterEngine engine) {
+        GeneratedPluginRegistrant.registerWith(engine);
+        final ShimPluginRegistry registry = new ShimPluginRegistry(engine);
+        PlatformPlugin.registerWith(registry.registrarFor("ml.cerasus.pics"));
     }
 }
