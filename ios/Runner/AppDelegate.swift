664631522ea81c668b9d4326c282b372b64771fa
神楽坂花火 <i@yaerin.com>
2020-03-06 22:26:27 +0800

    Merge branch 'today-extension'

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,3 +1,19 @@
+//
+// Copyright 2019 KagurazakaHanabi<i@yaerin.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
 import Flutter
 import Photos
 import UIKit
@@ -68,7 +84,7 @@ class AppDelegate: FlutterAppDelegate {
             result(nil)
         } else {
             let url = "itms-apps://itunes.apple.com/app/id1457009047?action=write-review"
-            UIApplication.shared.openURL(URL.init(string: url)!)
+            UIApplication.shared.open(URL.init(string: url)!)
             result(nil)
         }
     }
@@ -79,7 +95,7 @@ class AppDelegate: FlutterAppDelegate {
     }
 
     private func openAppSettings(result: FlutterResult) {
-        UIApplication.shared.openURL(URL.init(string: UIApplication.openSettingsURLString)!)
+        UIApplication.shared.open(URL.init(string: UIApplication.openSettingsURLString)!)
         result(nil)
     }
 
